5ac407e324fb424509ff838704832e12c6ed65c5
卖男孩的小火柴 <913648653@qq.com>
2024-01-12 21:31:15 +0800

    feat: init toolit

---
+++ src/extension.ts
@@ -0,0 +1,14 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log("lafjs", "恭喜你，你的插件已激活！");
+
+  const lafjsExec = vscode.commands.registerCommand("laf-toolkit.exec", async () => {
+  
+    vscode.window.setStatusBarMessage("");
+  });
+
+  context.subscriptions.push(lafjsExec);
+}
+// This method is called when your extension is deactivated
+export function deactivate() {}
